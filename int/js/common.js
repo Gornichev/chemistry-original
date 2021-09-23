5fc2da42eb9097a566edd7975904ebfe943aa25e
igornikolaevich <igorroschak@gmail.com>
2021-09-23 09:40:15 +0300

    fix menu style

--- int/js/common.js
+++ int/js/common.js
@@ -1,16 +1,16 @@
-const btnMenu = document.querySelector("#btn-menu");
-const hideMenu = document.querySelector("#hide-menu");
+const btnMenu = document.querySelector("#btn-show-menu");
+const hideMenu = document.querySelector("#btn-hide-menu");
 const body = document.querySelector("body");
 const menu = document.querySelector("#menu");
 
 btnMenu.onclick = function() {
-    // btnMenu.classList.add("hide");
-    body.classList.add("menu-show");
+    btnMenu.classList.add("hide");
+    body.classList.add("overlay");
     menu.classList.add("show");
 }
 
 hideMenu.onclick = function() {
     btnMenu.classList.remove("hide");
-    body.classList.remove("menu-show");
+    body.classList.remove("overlay");
     menu.classList.remove("show");
 }
